c9904ae6eeeb6626147702a949c6a9ce9592091c
Sabbir <s.sabbirhossain18@gmail.com>
2024-02-27 10:02:42 +0600

    product array add

--- src/exam1/src/components/product.tsx
+++ src/exam1/src/components/product.tsx
@@ -1,27 +1,39 @@
 import React from "react";
 import './product.css'
 import img1 from './asset/tide.png'
-const Product = () => {
 
+const products = [
+  {
+    id: 1,
+    name: "Tide Provider",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, corrupti?",
+    image: img1,
+  },
+  {
+    id: 2,
+    name: "Tide Provider",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, corrupti?",
+    image: img1,
+  }
+
+];
+
+const Product = () => {
   return (
-   <div className="container">
-        <h1>E Commerce</h1>
-    <div className="product">
-        <div className="itemleft"><img src={img1} alt=""/></div>
-        <div className="itemRight">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, corrupti?
+    <div className="container">
+      <h1>E Commerce</h1>
+      {products.map(product => (
+        <div className="product" key={product.id}>
+          <div className="itemleft"><img src={product.image} alt={product.name} /></div>
+          <div className="itemRight">
+            <h4>{product.name}</h4>
+            <p>{product.description}</p>
+        
+          </div>
         </div>
+      ))}
+      <br />
     </div>
-    <br />
-    <div className="product">
-        <div className="itemleft"><img src={img1} alt=""/></div>
-        <div className="itemRight">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, corrupti?
-        </div>
-    </div>
-    <br />
-   </div>
-   
   );
 };
 
